04e966cb5a74a2a2dda5b1a7d0e146c74ec2c1d2
Larry <90505134+soloplayer345@users.noreply.github.com>
2025-03-16 16:30:01 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   const pathname = usePathname();
-  const excludedPaths = ["/login", "/register", "/forgotpassword", "/admin"];
+  const excludedPaths = ["/login", "/register", "/forgotpassword", "/admin","/admin/blog","/admin/account","/admin/schedule-template","/admin/membership-plan","/admin/preganacy-standard"];
 
   return (
     <html lang="en">

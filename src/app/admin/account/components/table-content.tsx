28774a8107fc9beb36b5382bf8676dabf34eb55f
soloplayer345 <hongtlse172336@fpt.edu.vn>
2025-03-21 21:48:42 +0700

    Update table-content.tsx

--- src/app/admin/account/components/table-content.tsx
+++ src/app/admin/account/components/table-content.tsx
@@ -235,7 +235,7 @@ export const TableContent =() =>
                         Cancel
                     </Button>
                     <Button onClick={() => handleUpdateAccount(selectedAccount?.id || 0)} variant="contained" color="primary">
-                        Update {selectedAccount?.id}
+                        Update
                     </Button>
                 </DialogActions>
             </Dialog>
